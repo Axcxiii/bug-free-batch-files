d589386322fbfb9033d87df6c6297048e25740f6
Axcxiii <77469076+Axcxiii@users.noreply.github.com>
2025-06-23 16:43:00 -0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
 mshta javascript:alert("Dude.");close();
-mshta javascript:alert("Don't connect to random Bluetooth devices.");close();
+mshta javascript:alert("Not fucking cool.");close();
 curl https://raw.githubusercontent.com/Axcxiii/bug-free-batch-files/refs/heads/main/madeline.png > madeline.png
 start madeline.png
